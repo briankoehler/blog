4bd08fb8664c22ba4e97237966cbf76ef961e071
briankoehler <briankoehler@ufl.edu>
2021-02-22 09:53:17 -0500

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  siteMetadata: {
+    title: "Blog",
+  },
+  plugins: [
+    "gatsby-plugin-styled-components",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+  ],
+};
